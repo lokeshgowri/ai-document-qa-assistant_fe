0a689808f37ceae8cba85f1206f9201da69b937e
Lokeswar Reddy Gowri <lokeshgowri78@gmail.com>
2026-09-08 11:34:42 +0530

    Upadted URL

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 console.log("🚀 APP.JS LOADED");
 
-const API_BASE_URL = "https://letting-karaoke-arms-played.trycloudflare.com";
+const API_BASE_URL = "https://ai-document-assistant-nu.vercel.app";
 
 // ==========================================
 // HTML ELEMENTS
