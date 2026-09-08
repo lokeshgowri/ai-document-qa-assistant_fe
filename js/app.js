ab556bc887a3f22ad666e4b1ed9e383cc87f605e
Lokeswar Reddy Gowri <lokeshgowri78@gmail.com>
2026-09-08 16:53:22 +0530

    Upadted URL

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:8000"; // Update this to your backend API URL
+const API_BASE_URL = "https://ai-document-assistant-nu.vercel.app/"; // Update this to your backend API URL
 
 // ========================================
 // STATE
